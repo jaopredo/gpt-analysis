46189e1a77753699450d36abf19cd8af82af7045
alexoliveiraFGV24 <iamsralex@gmail.com>
2024-06-22 15:51:00 -0300

    Corriji o nome dos datasets

--- analytics.py
+++ analytics.py
@@ -8,6 +8,6 @@ def generate_analytics(dataset: pd.DataFrame):
     """
         Função que gera as análises necessárias para criação de gráficos específicos
     """
-    # analyse_comments(dataset)
+    analyse_comments(dataset)
 
-    generate_timeline(dataset)
+    #generate_timeline(dataset)
--- main.py
+++ main.py
@@ -10,12 +10,18 @@ def main():
     ...
 
     # Passando os dados
-    dataset = pd.read_csv('data/steam.csv', on_bad_lines="skip", sep=";")
+    dataset_steam = pd.read_csv('data/steam.csv', on_bad_lines="skip", sep=";")
+    dataset_imdb = pd.read_csv('data/steam.csv', on_bad_lines="skip", sep=";")
+
     
-    generate_analytics(dataset)
+    generate_analytics(dataset_steam)
+    generate_analytics(dataset_imdb)
+
 
     # Gerando o novo dataset
-    dataset.to_csv('data/new_steam.csv', sep=';')
+    dataset_steam.to_csv('data/new_steam.csv', sep=';')
+    dataset_imdb.to_csv('data/new_steam.csv', sep=';')
+
 
 
 if __name__ == "__main__":
